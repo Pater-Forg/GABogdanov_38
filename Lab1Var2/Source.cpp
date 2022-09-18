3a932acbb59930b6d220e83183ba334bc8fcb5d4
Pater-Forg <gbogdanovg@gmail.com>
2022-09-18 21:13:06 +0300

    Revert "Комментарии переведены на английский."
    This reverts commit a2b4ed1c67df18a83943eefd5033dde873d4a83d.

--- Lab1Var2/Source.cpp
+++ Lab1Var2/Source.cpp
@@ -3,55 +3,54 @@
 #include <ctime>     // time
 #include <map>       // map
 
-// ASCII code for english letter 'a'
+// ASCII код для буквы "a" английского алфавита
 const int ASCII_FIRST_LETTER_CODE_LC = 97;
-// ASCII code for english letter 'z'
+// ASCII код для буквы "z" английского алфавита
 const int ASCII_LAST_LETTER_CODE_LC = 122;
 
 /*
- * Generates random string from english letters (lower case)
+ * Генерирует строку из случайных символов английского алфавита (в нижнем регистре)
  * 
- * @param str Reference for string to fill
- * @param size Length of the generated string
+ * @param str ссылка на строку для заполнения
+ * @param size желаемая длина строки
  */
 void generateRandomString(std::string& str, int size);
 
 int main() {
-	// initialize random seed
+	// задаем семя для ГПСЧ
 	std::srand(std::time(nullptr));
-	// length of the generated string
+	// длина генерируемой строки
 	const int N = 10000;
-	// the string to fill
+	// строка для заполнения
 	std::string str = "";
-	// generate random string from english letters
-	std::cout << "Generating string..." << std::endl;
+	// генерируем строку из случайных символов английского алфавита
 	generateRandomString(str, N);
 
-	// associative container to store pair of <letter, number>
+	// ассоциативный контейнер для хранения пар <буква, количество>
 	std::map<char, int> letter_dict;
-	// fill the map
+	// заполняем словарь, устанавливая количество букв в 0
 	for (int code = ASCII_FIRST_LETTER_CODE_LC; code <= ASCII_LAST_LETTER_CODE_LC; code++) {
 		letter_dict.insert(std::pair<char, int>((char)code, 0));
 	}
-	// counting letters
-	std::cout << "Calculating..." << std::endl;
+
+	// считаем буковы
 	for (auto& letter : str) {
 		letter_dict[letter] += 1;
 	}
-	// print the result
+
+	// выводим результат
 	for (auto& pair : letter_dict) {
 		std::cout << pair.first << ": " << pair.second << std::endl;
 	}
-	std::cout << "Press <Enter> to quit" << std::endl;
-	std::getchar();
+
 	return 0;
 }
 
 void generateRandomString(std::string& str, int size) {
-	// clear string
+	// очищаем строку
 	str.clear();
 
-	// fill the string with english letters
+	// заполняем строку случайными символами английского алфавита
 	for (int i = 0; i < size; i++) {
 		int ascii_code = ASCII_FIRST_LETTER_CODE_LC +
 			(std::rand() % (ASCII_LAST_LETTER_CODE_LC - ASCII_FIRST_LETTER_CODE_LC + 1));
